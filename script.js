364b337295e476531106b1a41e4956d1041d692b
sharmanator <>
2021-05-28 13:19:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//variable to keep track of where in the light sequence the page is (current state)
+var sequenceNum = 0; 
+
+//function called when button is pressed
+function lightSequence(){
+  
+  //You will need an IF statement to check the current state and update the HTML elements accordingly using the pic variable.  
+ 
+}
